7c5cdd9489b73f1ca79b02cdaabfc9eeb9dc165b
vuanhtung10 <vuanhtung10@gmail.com>
2023-12-28 16:45:12 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller() // route /
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get() /// route "" / => api resfull api
+  @Render('home')
+  handleHomePage() {
+    const message = this.appService.getHello();
+    return {
+      message: message,
+    };
+  }
+  getHello() {
+    // return 'this.appService.getHello()';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://vuanhtung:1rYEWYdu8CsGw92j@cluster0.uw65mb8.mongodb.net/',
+    ),
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
